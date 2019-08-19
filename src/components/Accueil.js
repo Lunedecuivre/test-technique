00a8e00f508b63dc28e8d1b711e872baf02eaa05
lunedecuivre <lena.defirmas@gmail.com>
2019-08-19 16:20:16 +0200

    first

---
+++ src/components/Accueil.js
@@ -0,0 +1,211 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Accueil extends Component {
+  state = {
+    loading: true,
+    data: []
+  };
+
+  componentDidMount() {
+    axios.get(`http://localhost:3000/src/components/users.json`).then(res => {
+      this.setState({ data: res.data, loading: false });
+    });
+  }
+
+  render() {
+    if (this.state.loading === true) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <div className="page1">
+          {this.state.data.map((value, index) => {
+            return (
+              <div className="page1container">
+                <img src="../../files/header_background.jpg" />
+                <h1>Lorem Ipsum</h1>
+                <h3>Le lorem ipsum blabla</h3>
+
+                <div id="table">
+                  <div className="limiter">
+                    <div className="container-table100">
+                      <div className="wrap-table100">
+                        <div className="table100">
+                          <table>
+                            <thead>
+                              <tr className="table100-head">
+                                <th className="column1">
+                                  Nom <i className="fas fa-sort-down" />
+                                </th>
+                                <th className="column2">
+                                  Email <i className="fas fa-sort-down" />
+                                </th>
+                                <th className="column3">
+                                  Âge <i className="fas fa-sort-down" />
+                                </th>
+                                <th className="column4">
+                                  Ville <i className="fas fa-sort-down" />
+                                </th>
+                                <th className="column5">Plus</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td className="column1">
+                                  {this.state.users.map(user => (
+                                    <p>{user.firstname}</p>
+                                  ))}
+                                </td>
+                                <td className="column2">200398</td>
+                                <td className="column3">iPhone X 64Gb Grey</td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-28 05:57</td>
+                                <td className="column2">200397</td>
+                                <td className="column3">Samsung S8 Black</td>
+                                <td className="column4">$756.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-26 05:57</td>
+                                <td className="column2">200396</td>
+                                <td className="column3">
+                                  Game Console Controller
+                                </td>
+                                <td className="column4">$22.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-25 23:06</td>
+                                <td className="column2">200392</td>
+                                <td className="column3">USB 3.0 Cable</td>
+                                <td className="column4">$10.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-24 05:57</td>
+                                <td className="column2">200391</td>
+                                <td className="column3">
+                                  Smartwatch 4.0 LTE Wifi
+                                </td>
+                                <td className="column4">$199.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-23 05:57</td>
+                                <td className="column2">200390</td>
+                                <td className="column3">Camera C430W 4k</td>
+                                <td className="column4">$699.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-22 05:57</td>
+                                <td className="column2">200389</td>
+                                <td className="column3">
+                                  Macbook Pro Retina 2017
+                                </td>
+                                <td className="column4">$2199.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-21 05:57</td>
+                                <td className="column2">200388</td>
+                                <td className="column3">
+                                  Game Console Controller
+                                </td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-19 05:57</td>
+                                <td className="column2">200387</td>
+                                <td className="column3">iPhone X 64Gb Grey</td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-18 05:57</td>
+                                <td className="column2">200386</td>
+                                <td className="column3">iPhone X 64Gb Grey</td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-22 05:57</td>
+                                <td className="column2">200389</td>
+                                <td className="column3">
+                                  Macbook Pro Retina 2017
+                                </td>
+                                <td className="column4">$2199.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-21 05:57</td>
+                                <td className="column2">200388</td>
+                                <td className="column3">
+                                  Game Console Controller
+                                </td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-19 05:57</td>
+                                <td className="column2">200387</td>
+                                <td className="column3">iPhone X 64Gb Grey</td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                              <tr>
+                                <td className="column1">2017-09-18 05:57</td>
+                                <td className="column2">200386</td>
+                                <td className="column3">iPhone X 64Gb Grey</td>
+                                <td className="column4">$999.00</td>
+                                <td className="column5">
+                                  <i className="fas fa-plus-circle" />
+                                </td>{" "}
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+  }
+}
+
+export default Accueil;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Accueil from "./components/Accueil";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Accueil source="./users.json" />,
+  document.getElementById("accueil")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
